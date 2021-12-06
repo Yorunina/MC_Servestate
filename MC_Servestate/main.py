9cc880bf2462c38601fc7780e82a8d0374eb402d
Yorunina <836193631@qq.com>
2021-12-06 19:13:52 +0800

    Update main.py

--- MC_Servestate/main.py
+++ MC_Servestate/main.py
@@ -8,6 +8,7 @@ import time;
 platform_bot_info = {}
 listen_player_list = []
 
+
 class Event(object):
     def init(plugin_event, Proc):
         bot_dict = Proc.Proc_data['bot_info_dict']
@@ -28,6 +29,29 @@ class Event(object):
         pass
 
 
+class Group:
+    def __init__(self, platform, group, channel):
+        self.platform = platform
+        self.group = group
+        self.channel = channel
+        return
+
+class Radio:
+    def __init__(self):
+        self.group = []
+        with open(data_file + "/radio_list.json",'r',encoding='utf8')as f:
+            for i in json.load(f):
+                group_obj = Group(i["platform"],i["group"],i["channel"])
+                self.group.append(group_obj)
+        return
+    def all(self, msg):
+        for group in self.group:
+            if group.platform == "dodo":
+                bot_info = platform_bot_info["dodo"]
+                plugin_event_fake = OlivOS.API.Event(OlivOS.contentAPI.fake_sdk_event(bot_info))
+                plugin_event_fake.send('group', group.group, "测试", group.channel)
+        return
+
 class Player:
     def __init__(self, name, ts):
         self.begin_time = ts
@@ -44,6 +68,7 @@ class Serve:
 class Process:
     def __init__(self):
         self.serve_list = []
+        self.serve_data = []
         with open(data_file + "/serve_list.json",'r',encoding='utf8')as f:
             self.serve_data = json.load(f)
             for i in self.serve_data:
@@ -74,7 +99,7 @@ class Process:
 
     def del_data(self, num):
         #删除对应的数据
-        if num > len(self.serve_list):
+        if num >= len(self.serve_list):
             return False
         serve_obj = self.serve_list[num]
         self.serve_list.pop(num)
@@ -102,26 +127,29 @@ class Process:
             return "未知，原因：\n服务器状态异常！"
         return "\n".join(player_list)
 
-    def listen_player(self):
+    def listen_player(self,  group_radio = True):
         #监听玩家列表
         now_listen_list = []
         for i in range(0, len(self.serve_list)):
-            if i > len(listen_player_list):
+            if i >= len(listen_player_list):
                 #如果是新的id，则新建一个字典
                 listen_player_list[i] = {}
+                #禁用本次的广播
+                group_radio = False
             player_amount, player_list = self.get_player(self.serve_list[i])
             now_listen_list[i] = player_list
             #比对新旧列表
-            old_player_list = listen_player_list[i]
-            listen_player_list[i] = []
+            new_player_list = []
+            ts = time.time()
             for player in player_list:
-                if i in old_player_list:
-                    old_player_list.remove(player)
-                    continue
+                if player in listen_player_list[i]:
+                    new_player_list[player] = listen_player_list[i][player]
                 else:
-                    ts = time.time()
                     #记录开始游玩时间
-                    listen_player_list[i][player] = Player(player, ts)
+                    new_player_list[player] = Player(player, ts)
+                    if group_radio:
+                        Radio.all("%s正在游玩：%s" % player,self.serve_list[i])
+            listen_player_list[i] = new_player_list[:]
 
             
         return
@@ -138,20 +166,21 @@ class LoopTimer(threading.Timer):
             self.function(*self.args, **self.kwargs)
 
 def listen_loop():
-    bot_info = platform_bot_info["dodo"]
-    plugin_event_fake = OlivOS.API.Event(OlivOS.contentAPI.fake_sdk_event(bot_info))
-    plugin_event_fake.send('group', 131690, "测试", 108193)
+    
     return
 
 if __name__ != "__main__":
     data_file = "./plugin/data/MC_Servestate"
     if not os.path.exists(data_file):
         os.mkdir(data_file)
-    if not os.access(data_file + "/serve_list.json", os.F_OK):
-        f = open(data_file + "/serve_list.json","w",encoding='utf8')
-        f.write("[]")
-        f.close()
+    sub_files = ["/serve_list.json", "/radio_list.json"]
+    for sub_file in sub_files:
+        if not os.access(data_file + sub_file, os.F_OK):
+            f = open(data_file + sub_file,"w",encoding='utf8')
+            f.write("[]")
+            f.close()
     process_obj = Process()
+    process_obj.listen_player(False)
     t = LoopTimer(30, listen_loop)
     t.start()
 
@@ -176,7 +205,7 @@ def unity_reply(plugin_event, Proc):
     if mat_det:
         plugin_event.reply("当前在线玩家：\n" + process_obj.player_detail(mat_det.group(1)))
     if mat_del:
-        num = mat_del.group(1)
+        num = int(mat_del.group(1))
         serve_obj = process_obj.del_data(num)
         if serve_obj:
             plugin_event.reply("成功删除[%d](%s)服务器！" % num,serve_obj.name)
